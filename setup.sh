ec674e7db9f264dfb14d82660d08d62dc2e64aff
Chris Sprague <chris@chaoscontrol.org>
2018-07-22 23:24:14 -0400

    Add colors

--- setup.sh
+++ setup.sh
@@ -5,32 +5,87 @@
 readonly HOMEDIR="/Users/chris"
 readonly HOSTNAME="robot-node-04"
 
+# --- Colors
+
+black="\033[0;30m"
+white="\033[0;37m"
+red="\033[0;31m"
+green="\033[0;32m"
+yellow="\033[0;33m"
+blue="\033[0;34m"
+magenta="\033[0;35m"
+cyan="\033[0;36m"
+
+# Color-echo. Improved. [credit to @joaocunha]
+# arg $1 = message
+# arg $2 = Color
+cecho() {
+  echo "${2}${1}${reset}"
+  return
+}
+
+ # --- Get Started
+
+# Set continue to false by default
+CONTINUE=false
+
+echo ""
+cecho "###############################################" $red
+cecho "#        DO NOT RUN THIS SCRIPT BLINDLY       #" $red
+cecho "#         YOU'LL PROBABLY REGRET IT...        #" $red
+cecho "#                                             #" $red
+cecho "#              READ IT THOROUGHLY             #" $red
+cecho "#         AND EDIT TO SUIT YOUR NEEDS         #" $red
+cecho "###############################################" $red
+echo ""
+
+echo ""
+cecho "Have you read through the script you're about to run and " $red
+cecho "understood that it will make changes to your computer? (y/n)" $red
+read -r response
+
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  CONTINUE=true
+  echo "OK... Here we go!"
+fi
+
+if ! $CONTINUE; then
+  # Check if we're continuing and output a message if not
+  cecho "Please go read the script, it only takes a few minutes" $red
+  exit
+fi
+
 # --- Unix Environment
 
 # Dot Files
 
+cecho "###############################################" $blue
 echo ""
 echo "Cloning .dotfiles repo..."
 echo ""
+
 git clone https://github.com/bad-mushroom/dotfiles.git $HOMEDIR/.dotfiles
 $HOMEDIR/.dotfiles/setup.sh
-echo "done."
+
+cecho "Done." $green
 
 # Directories
 
+cecho "###############################################" $blue
 echo ""
 echo "Adding custom directories..."
 echo ""
 
-mkdir $HOMEDIR/.ssh/keys/public     ## SSH Public Key Store
-mkdir $HOMEDIR/.ssh/keys/private    ## SSH Private Key Store
-mkdir $HOMEDIR/Projects             ## Code Projects
-mkdir $HOMEDIR/tmp                  ## Misc Crap
+mkdir -p $HOMEDIR/.ssh/keys/public     ## SSH Public Key Store
+mkdir -p $HOMEDIR/.ssh/keys/private    ## SSH Private Key Store
+mkdir -p $HOMEDIR/Projects             ## Code Projects
+mkdir -p $HOMEDIR/tmp                  ## Misc Crap
 
-echo "done."
+cecho "Done." $green
 
 # sudo
 
+cecho "###############################################" $blue
 echo ""
 echo "From here on we need root access. Enter your password."
 echo ""
@@ -40,6 +95,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Hostname
 
+cecho "###############################################" $blue
 echo ""
 echo "Setting hostname to $HOSTNAME"
 echo ""
@@ -49,20 +105,22 @@ sudo scutil --set HostName $HOSTNAME
 sudo scutil --set LocalHostName $HOSTNAME
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $HOSTNAME
 
-echo "done."
+cecho "Done." $green
 
 # --- MacOS Updates
 
+cecho "###############################################" $blue
 echo ""
 echo "Installing MacOS updates:"
 echo ""
 
 softwareupdate --install --all
 
-echo "done."
+cecho "Done." $green
 
 # --- GCC/Xcode Tools
 
+cecho "###############################################" $blue
 echo ""
 echo "Checking for Xcode..."
 echo ""
@@ -72,11 +130,21 @@ if [[ ! -e `which gcc` || ! -e `which gcc-4.2` ]]; then
 	xcode-select --install
 fi
 
-echo "done."
+cecho "Done." $green
 
+cecho "###############################################" $blue
+echo ""
+echo "Enabling Firewall..."
+echo ""
+
+# Enable Filevault
+fdesetup enable
+
+cecho "Done." $green
 
 # --- MacOS Preferences
 
+cecho "###############################################" $blue
 echo ""
 echo "Setting Mac OS preferences..."
 echo ""
@@ -85,6 +153,18 @@ echo ""
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Finder
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set DesktopViewSettings:IconViewSettings:labelOnBottom false" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+chflags nohidden ~/Library/
 
 # Show battery percentage/time remaining
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
@@ -120,15 +200,23 @@ defaults write com.apple.screencapture type jpg
 for app in Finder Dock; do killall "$app"; done
 killall SystemUIServer
 
-echo "done."
+cecho "Done." $green
 
 # --- Homebrew Package Manger
 
+cecho "###############################################" $blue
 echo ""
 echo "Installing Homebrew..."
 echo ""
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+cecho "Done." $green
+
+cecho "###############################################" $blue
+echo ""
+echo "Installing apps..."
+echo ""
 
 apps=(
 	atom				# Code Editor
@@ -149,11 +237,25 @@ apps=(
 brew cask install ${apps[@]}
 brew cleanup
 
-# --- Misc
+# --- Done!
 
-# Enable Filevault
-fdesetup enable
+echo ""
+echo ""
+echo "That's all folks..."
+echo ""
+echo ""
+cecho "################################################################################" $white
+echo ""
+echo ""
+cecho "Note that some of these changes require a logout/restart to take effect." $red
+cecho "Killing some open applications in order to take effect." $red
+echo ""
 
+find ~/Library/Application\ Support/Dock -name "*.db" -maxdepth 1 -delete
+for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+  "Dock" "Finder" "Mail" "Messages" "Safari" "SystemUIServer"; do
+  killall "${app}" > /dev/null 2>&1
+done
 
 # Exit root shell
 exit
